063dcf376df85d9d52112e03098235a7dd2bb2b7
Aleks K. <a.s.kovachev@gmail.com>
2025-12-10 21:20:30 +0200

    * Removed redundant MainWindow::RenderFrame() function declaration. * Changed geometry color change speed in PixelShader.

--- WolfRenderer/shaders/ConstColor.hlsl
+++ WolfRenderer/shaders/ConstColor.hlsl
@@ -8,7 +8,7 @@ float4 PSMain( PSInput input ) : SV_TARGET {
     float4 redColor = float4( 1.0, 0.0, 0.0, 1.0 );
     float4 purpleColor = float4( 0.5, 0.0, 0.5, 1.0 );
 
-    if ( frameIdx % 100 >= 0 && frameIdx % 100 <= 50 ) {
+    if ( frameIdx % 800 <= 400 ) {
         return redColor;
     } else {
         return purpleColor;
