82ff27fb9e643439e85c646efdfb6d3c9d77c544
Aleks K. <a.s.kovachev@gmail.com>
2025-12-23 22:48:14 +0200

    ray_tracing_shaders.hlsl changed to incorporate camera ray shot into the scene logic (beginning of ray tracing).

--- WolfRenderer/shaders/ray_tracing_shaders.hlsl
+++ WolfRenderer/shaders/ray_tracing_shaders.hlsl
@@ -2,15 +2,72 @@ RWTexture2D<float4> frameTexture : register( u0 );
 
 [shader("raygeneration")]
 void rayGen() {
-    uint2 pixelCoord = DispatchRaysIndex().xy;
+    float width = 800.f;
+    float height = 800.f;
+
+    RayDesc cameraRay;
+    cameraRay.Origin = float3( 0.f, 0.f, 0.f );
+
+    uint2 pixelRasterCoord = DispatchRaysIndex().xy;
 
     // Fill the frame with blue color on hit.
-    frameTexture[pixelCoord] = float4( 0.0, 0.0, 1.0, 1.0 );
+    // frameTexture[pixelRasterCoord] = float4( 0.0, 0.0, 1.0, 1.0 );
 
-    // uint2 screenSize = DispatchRaysDimensions().xy;
-    // float2 uv = (pixelCoord + 0.5) / screenSize;
-    // float3 color = float3(uv, 0.5);
-    // frameTexture[pixelCoord] = float4(color, 1.0);
+    float x = pixelRasterCoord.x;
+    float y = pixelRasterCoord.y;
+
+    // Create a simple gradient based on pixel coordinates.
+    // float rasterR = ( x % 256 ) / 255.f;
+    // float rasterG = ( y % 256 ) / 255.f;
+    // cameraRay.Direction = float3( rasterR, rasterG, 0.f );
+    // frameTexture[pixelRasterCoord] = float4( cameraRay.Direction, 1.f );
+
+    // Center the coordinates to the pixel.
+    x += 0.5f;
+    y += 0.5f;
+
+    // Normalize to NDC [0, 1].
+    x /= width;
+    y /= height;
+
+    // Distribute gradient to all pixels for full screen gradient.
+    // cameraRay.Direction = float3( x, y, 0.f );
+    // frameTexture[pixelRasterCoord] = float4( cameraRay.Direction, 1.f );
+
+    // NDC to screen space [-1, 1].
+    x = ( 2.f * x ) - 1.f;
+    y = 1.f - ( 2.f * y ); // Flip Y axis.
+
+    // Put the blue channel (black color) in the center. Red - horizontal, Green - vertical.
+    // float screenR = abs( x );
+    // float screenG = abs( y );
+    // cameraRay.Direction = float3( screenR, screenG, 0.f );
+    // frameTexture[pixelRasterCoord] = float4( cameraRay.Direction, 1.f );
+
+    x *= width / height; // Adjust for aspect ratio.
+
+    // float aspectR = abs( x );
+    // float aspectG = abs( y );
+    // cameraRay.Direction = float3( aspectR, aspectG, 0.f );
+    // frameTexture[pixelRasterCoord] = float4( cameraRay.Direction, 1.f );
+
+    // Set the Z component to -1 to point into the scene.
+    float3 rayDirection = float3( x, y, -1.f );
+
+    // float rayDirR = abs( rayDirection.x );
+    // float rayDirG = abs( rayDirection.y );
+    // float rayDirB = abs( rayDirection.z );
+    // cameraRay.Direction = float3( rayDirR, rayDirG, rayDirB );
+    // frameTexture[pixelRasterCoord] = float4( cameraRay.Direction, 1.f );
+
+    float3 rayDirectionNormalized = normalize( rayDirection );
+    cameraRay.Direction = rayDirectionNormalized;
+
+    float r = abs( cameraRay.Direction.x );
+    float g = abs( cameraRay.Direction.y );
+    float b = abs( cameraRay.Direction.z );
+
+    frameTexture[pixelRasterCoord] = float4( r, g, b, 1.f );
 }
 
 struct RayPayload {
